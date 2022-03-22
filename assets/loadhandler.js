adf72fb3c57381dff6a03a59269cffb5d4358ea5
bizkiwi <steve@bizkiwi.nz>
2022-03-23 01:03:02 +1300

    Update loadhandler.js

--- assets/loadhandler.js
+++ assets/loadhandler.js
@@ -5,20 +5,28 @@ function LoadHandler() {
 
 function ShowQRcode() {
 	var selector = document.getElementById("qrcode-modal").style;
-	selector.position = "absolute";
-	selector.zIndex = "20000";
-	selector.display = "block";
-	selector.visibility = "visible";
-	selector.left = "0vw";
-	//selector.right = "150vw";
-	//selector.top = "-50vh";
-	selector.bottom = "0vh";
-	selector.width = "100vw";
-	selector.height = "100vh";
-	selector.backgroundColor = "#000000";
-	selector.padding = "auto";
-
-	var selector = document.getElementById("qrcode-image").style;
-	selector.zIndex = "20000";
+	if(selector.visibility == "visible") {
+		selector.position = "absolute";
+		selector.zIndex = "-20000";
+		selector.display = "none";
+		selector.visibility = "hidden";
+		var selector = document.getElementById("qrcode-image").style;
+		selector.zIndex = "-20000";
+	} else {
+		selector.position = "absolute";
+		selector.zIndex = "20000";
+		selector.display = "block";
+		selector.visibility = "visible";
+		selector.left = "0vw";
+		//selector.right = "150vw";
+		//selector.top = "-50vh";
+		selector.bottom = "0vh";
+		selector.width = "100vw";
+		selector.height = "100vh";
+		selector.backgroundColor = "#000000";
+		selector.padding = "auto";
+		var selector = document.getElementById("qrcode-image").style;
+		selector.zIndex = "20000";
+	}
 	Console.log("ShowQRcode DONE");
 }
