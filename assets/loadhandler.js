bf1537aec3e7b691b75e3dc8d9467776f51521a1
bizkiwi <steve@bizkiwi.nz>
2022-03-22 23:07:56 +1300

    Update loadhandler.js

--- assets/loadhandler.js
+++ assets/loadhandler.js
@@ -7,17 +7,17 @@ function ShowQRcode() {
 	var selector = document.getElementById("qrcodemodal").style;
 	selector.position = "absolute";
 	selector.zIndex = "20000";
-	selector.width = "200vw";
-	selector.height = "200vh";
+	selector.display = "block";
+	selector.visibility = "visible";
 	selector.left = "-50vw";
 	selector.right = "150vw";
 	selector.right = "-50vh";
-	selector.bottom = "-50vh";
+	selector.bottom = "-150vh";
+	selector.width = "200vw";
+	selector.height = "200vh";
 	selector.backgroundColor = "#000000";
 	selector.padding = "auto";
-	selector.display = "block";
-	selector.visibility = "visible";
-	
+
 	var selector = document.getElementById("qrcode-image").style;
 	selector.zIndex = "20000";
 	Console.log("ShowQRcode DONE");
