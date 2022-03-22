03d19c32cd50dda411a996b5d09d504562451ee2
bizkiwi <steve@bizkiwi.nz>
2022-03-22 21:14:29 +1300

    Update

--- assets/loadhandler.js
+++ assets/loadhandler.js
@@ -2,14 +2,15 @@ function LoadHandler() {
 	delay(20000);
 	var selector = document.getElementById("#qrcodemodal").style;
 	selector.position = "absolute";
-	selector.display = "block";
 	selector.zIndex = "20000";
 	selector.width = "100vw";
 	selector.height = "100vh";
 	selector.left = "0px";
 	selector.top = "0px";
 	selector.background-color = "#000000";
-	selector.visibility = "visible";
 	selector.margin = "0px";
 	selector.padding = "0px";
+	selector.display = "block";
+	selector.visibility = "visible";
+	Console.log("DDONE");
 }
